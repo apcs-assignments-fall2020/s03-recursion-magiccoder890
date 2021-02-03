1fac6b9d1113af1b6f18d00010cca537016e0597
magiccoder890 <23fzhu@athenian.org>
2021-02-03 13:06:10 +0000

    Work

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,7 +3,12 @@ public class MyMain {
     // in x, recursively
     public static int addDigits(int x) { 
         // YOUR CODE HERE
-        return -1;
+        if (x % 10 == x){
+            return x;
+        }
+        else {
+        return x % 10 + addDigits(x/10);
+        }
     }
 
 
@@ -11,14 +16,22 @@ public class MyMain {
     // (e.g. "racecar", "madam"), recursively
     public static boolean isPalindrome(String str) { 
         // YOUR CODE HERE
-        return false;
+        String new_str = "";
+        for (int i = str.length() - 1; i > -1 ;i --){
+            new_str += str.charAt(i);
+        }
+        return str.equals(new_str);
+
     }
 
     // This method returns the orignal string reversed;
     // this method should be written using recursion
     public static String reverse(String str) { 
         // YOUR CODE HERE
-        return "";
+        if (str.length() == 0)
+            return str;
+        return reverse(str.substring(1)) + str.charAt(0);
+        
     }
 
     public static void main(String[] args) {
